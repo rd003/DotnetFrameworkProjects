5df5c8086058fd9256b4bb402fd5b5f7ff77626c
Ravindra Devrani <ravindra.devrani003@gmail.com>
2024-05-14 22:22:45 +0530

    feat: todo mvc

---
+++ TodoApp/TodoApp.Web/Controllers/TodoController.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using TodoApp.Data.Models;
+using TodoApp.Data.Repositories;
+
+namespace TodoApp.Web.Controllers
+{
+    public class TodoController : Controller
+    {
+        private readonly ITodoRepository _todoRepository;
+
+        public TodoController(ITodoRepository todoRepository)
+        {
+            _todoRepository = todoRepository;
+        }
+        public async Task<ActionResult> Index()
+        {
+            try
+            {
+                var todos = await _todoRepository.GetTodosAsync();
+                ViewBag.Todos = todos;
+                return View();
+            }
+            catch(Exception ex)
+            {
+                return View();
+            }
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Index(Todo todo)
+        {
+            if(!ModelState.IsValid)
+            {
+                return View(todo);
+            }
+            try
+            {
+                await _todoRepository.AddTodoAsync(todo);
+                return RedirectToAction(nameof(Index));
+            }
+            catch (Exception ex)
+            {
+                return View();
+            }
+        }
+
+        public async Task<ActionResult> ToggleItem(int id)
+        {
+            try
+            {
+                var todoItem = await _todoRepository.FindTodByIdAsync(id);
+                //if(todoIem==null)
+                //{
+
+                //}
+                todoItem.Completed=!todoItem.Completed;
+                await _todoRepository.UpdateTodoAsync(todoItem);
+                return RedirectToAction(nameof(Index));
+            }
+            catch (Exception ex)
+            {
+                return View();
+            }
+        }
+
+        public async Task<ActionResult> DeleteItem(int id)
+        {
+            try
+            {
+                var todoItem = await _todoRepository.FindTodByIdAsync(id);
+                //if(todoIem==null)
+                //{
+
+                //}
+                await _todoRepository.DeleteTodoAsync(todoItem);
+                return RedirectToAction(nameof(Index));
+            }
+            catch (Exception ex)
+            {
+                return View();
+            }
+        }
+
+    }
+}
--- TodoApp/TodoApp.Web/Global.asax.cs
+++ TodoApp/TodoApp.Web/Global.asax.cs
@@ -1,10 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using Autofac;
+using Autofac.Integration.Mvc;
 using System.Web.Mvc;
-using System.Web.Optimization;
 using System.Web.Routing;
+using TodoApp.Data.Models;
+using TodoApp.Data.Repositories;
 
 namespace TodoApp.Web
 {
@@ -15,7 +14,21 @@ namespace TodoApp.Web
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
-            BundleConfig.RegisterBundles(BundleTable.Bundles);
+            // BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            var builder = new ContainerBuilder();
+
+            // Register your MVC controllers. (MvcApplication is the name of
+            // the class in Global.asax.)
+            builder.RegisterControllers(typeof(MvcApplication).Assembly);
+
+            // Register types
+            builder.RegisterType<TodoRepository>().As<ITodoRepository>();
+            builder.RegisterType<ApplicationDbContext>().AsSelf().InstancePerRequest();
+
+            // Set the dependency resolver to be Autofac.
+            var container = builder.Build();
+            DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
         }
     }
 }
